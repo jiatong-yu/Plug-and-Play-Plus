72edb48e52459675b3c0279f732ef2aef5966ab2
Jiatong Yu <jiatongy@node210.ionic.cs.princeton.edu>
2022-12-01 17:08:31 -0500

    init

--- data_util.py
+++ data_util.py
@@ -1,8 +1,25 @@
 import datasets 
 from datasets import load_dataset, Dataset
+from torch.utils.data import TensorDataset, DataLoader, SequentialSampler
 import numpy as np
 import pandas as pd 
 import os 
+from tqdm import tqdm 
+import torch
+
+def get_dataloader(inputs, batch_size=64):
+    """
+    Return dataloader. for each batch, input_ids = batch[0] and attn_mask = batch[1]
+    """
+    shape = inputs["input_ids"].shape 
+    for v in inputs.values():
+        assert v.shape == shape 
+    
+    dataset = TensorDataset(inputs["input_ids"], inputs["attn_mask"])
+    sampler = SequentialSampler(dataset)
+    dataloader = DataLoader(dataset, sampler=sampler, batch_size=batch_size)
+    return dataloader
+    
 
 def load_data(logger, name="newsroom", split="validation", data_dir = "./data"):
     """
@@ -19,7 +36,67 @@ def load_data(logger, name="newsroom", split="validation", data_dir = "./data"):
     else: 
         raise NotImplementedError()
     
+    logger.info(f"loaded {len(data)} from {name}.")
     return data
     
+
+
+def prepare_data(logger,
+                    tokenizer,data_list, 
+                    title_prefix, content_prefix, 
+                    summary_prefix=None, 
+                    max_length=512):
+    """
+    Truncate each data to max_length and pad short samples, prepare data in tensor form.
+        data_list: Dataset, keys {title, summary, text}
+        title_prefix, content_prefix, summary_prefix: str 
+    
+    Prepare each data in the form title_prefix, title, (summary_prefix, summary), content_prefix.
+    
+    TODO: could optimize speed.
+
+    Return 
+        input_tensors: Dict[str, List] with keys {input_ids, attn_mask}
+            input_ids: tokenizer output of prompt 
+            attn_mask: mask out paddings
+        
+    """
+
+    bos_token_id = tokenizer.bos_token_id
+    eos_token_id = tokenizer.eos_token_id 
+
+    logger.info("preparing data tensors...")
+    
+    input_ids_list = []
+    attn_mask_list = []
+    for data in tqdm(data_list):
+        prompt = title_prefix.copy()+" "+data['title']+" "
+        if summary_prefix is not None: 
+            prompt = prompt + summary_prefix.copy()+" "+data['title']+" "
+        
+        prompt = prompt + content_prefix.copy()
+
+        logger.info(prompt)
+        
+        tokens = tokenizer(prompt)["input_ids"][:max_length-2]
+
+        tokens = [bos_token_id] + tokens 
+        tokens = tokens + [eos_token_id]
+
+        logger.info(f"length of tokens: {len(tokens)}")
+
+        n_pad = max_length - len(tokens)
+        input_ids = tokens + [0 for _ in range(n_pad)]
+        attn_mask = [1 for _ in tokens] + [0 for _ in range(n_pad)]
+
+        input_ids_list.append(input_ids)
+        attn_mask_list.append(attn_mask)
+    
+    return {
+        "input_ids": torch.LongTensor(input_ids_list),
+        "attn_mask": torch.LongTensor(attn_mask_list)
+    }
+        
+
 
 
--- model_util.py
+++ model_util.py
@@ -8,7 +8,10 @@ from transformers import GPT2Tokenizer, GPT2LMHeadModel
 from transformers import GPT2LMHeadModel
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
-def load_model(logger, model_name):
+def load_model(logger, model_name="gpt2"):
+    """
+    Load transformers model, return tokenizer and model. 
+    """
     free_in_GB = int(torch.cuda.mem_get_info()[0]/1024**3)
     max_memory = f'{free_in_GB-2}GB'
     n_gpus = torch.cuda.device_count()
@@ -18,16 +21,14 @@ def load_model(logger, model_name):
     # Load gpt2 model of different sizes 
     if "gpt2" in model_name:
         assert model_name in ["gpt2", "gpt2-medium","gpt2-large", "gpt2-xl"]
-        tokenizer = GPT2Tokenizer.from_pretrained(model_name, cache_dir = "/n/fs/nlp-jiatongy/iw22fall/base/.cache")
+        tokenizer = GPT2Tokenizer.from_pretrained(model_name)
         torch.cuda.empty_cache()
-        
         model = GPT2LMHeadModel.from_pretrained(
             model_name,
             device_map="auto",
             torch_dtype=torch.float16,
             max_memory=max_memory,
-            cache_dir = "/n/fs/nlp-jiatongy/iw22fall/base/.cache",
-            offload_folder = "/n/fs/nlp-jiatongy/iw22fall/base/.offload"
+            offload_folder = "~/.cache/huggingface/.offload"
             )
         return model, tokenizer
 
@@ -36,31 +37,30 @@ def load_model(logger, model_name):
         assert model_name in ["opt-350m", "opt-1.3b", "opt-2.7b", "opt-6.7b","opt-13b", "opt-30b", "opt-66b"]
         model_name = "facebook/"+model_name
         model = AutoModelForCausalLM.from_pretrained(
-        model_name,
-        device_map='auto',
-        torch_dtype=torch.float16,
-        max_memory=max_memory,
-        cache_dir ="/n/fs/nlp-jiatongy/iw22fall/base/.cache",
-        offload_folder = "/n/fs/nlp-jiatongy/iw22fall/base/.offload"
+            model_name,
+            device_map='auto',
+            torch_dtype=torch.float16,
+            max_memory=max_memory,
+            offload_folder = "~/.cache/huggingface/.offload"
         )
 
-        tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast=False, cache_dir="/n/fs/nlp-jiatongy/iw22fall/base/.cache")
+        tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast=False)
         return model, tokenizer 
     
     if "galactica" in model_name:
         assert model_name in ["galactica-125m","galactica-1.3b","galactica-6.7b",'galactica-30b']
         model_name = "facebook/"+model_name
         model = AutoModelForCausalLM.from_pretrained(
-        model_name,
-        device_map='auto',
-        torch_dtype=torch.float16,
-        max_memory=max_memory,
-        cache_dir ="/n/fs/nlp-jiatongy/iw22fall/base/.cache",
-        offload_folder = "/n/fs/nlp-jiatongy/iw22fall/base/.offload"
+            model_name,
+            device_map='auto',
+            torch_dtype=torch.float16,
+            max_memory=max_memory,
+            offload_folder = "~/.cache/huggingface/.offload"
         )
 
-        tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast=False, cache_dir="/n/fs/nlp-jiatongy/iw22fall/base/.cache")
+        tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast=False)
         return model, tokenizer 
         
 
     raise NotImplementedError("model input not supported.")
+
