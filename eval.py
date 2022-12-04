f369bf676bb76a98d2b757ee57aa637da33efec8
Jiatong Yu <jiatongy@node031.ionic.cs.princeton.edu>
2022-12-03 21:16:27 -0500

    baseline1

--- eval.py
+++ eval.py
@@ -7,6 +7,37 @@ from evaluate import load
 import logging
 from tqdm import tqdm
 
+def load_data_pair(logger, data_path):
+    """
+    Load generation results, convert reference to prompt format. 
+        data_path should contain res.npy and ref.npy files.
+    
+    TODO: if prompting method changed, this also need to be chagned.
+    """
+    logger.info(f"loading data from {data_path}")
+    assert os.path.exists(data_path+"-res.npy")
+    generations = np.load(data_path+"-res.npy")
+    references = np.load(data_path+"-ref.npy",allow_pickle=True)
+
+    assert len(generations) == len(references)
+    
+    labels=[]
+    for data in references:
+        if len(data["summary"]) > 1:
+            summary_chunck = data["summary"].split(sep="\n")[0].split(sep=". ")[0]+". "
+            prefix = "Generate long article based on title and summary. Title: "
+            prompt = prefix+data["title"]+" Summary: "+ summary_chunck+"Generation: "
+        
+        else: 
+            prefix = "Generate a long article based on title. Title: "
+            prompt = prefix+data["title"]+" Generation: "
+        
+        label = prompt+data["text"]
+        labels.append(label)
+    
+    return generations, labels
+
+
 def calc_stats(logger, generations, labels):
     """
     Calculate the average number of words and sentences.
@@ -35,60 +66,37 @@ def calc_stats(logger, generations, labels):
     return 
     
 
-def eval_mauve(logger,data_path):
+def eval_mauve(logger,generations, labels):
     """
     Return mauve score of generations and references.
-        data_path should contain a -ref.npy and a -res.npy as in run_baseline.py
     """
-    logger.info(f"loading data from {data_path}")
-    assert os.path.exists(data_path+"-res.npy")
-    generations = np.load(data_path+"-res.npy")
-    references = np.load(data_path+"-ref.npy",allow_pickle=True)
-
-    assert len(generations) == len(references)
-    
+    mauve = load("mauve")
     if len(generations)<200: 
         logger.warning("To run mauve score, need at least 200 data points.")
-    
-    """
-    TODO: if prompting method changed, this also need to be chagned.
-    """
-    
-
-    mauve = load("mauve")
-
-    labels=[]
-    for data in references:
-        if len(data["summary"]) > 1:
-            summary_chunck = data["summary"].split(sep="\n")[0].split(sep=". ")[0]+". "
-            prefix = "Generate long article based on title and summary. Title: "
-            prompt = prefix+data["title"]+" Summary: "+ summary_chunck+"Generation: "
-        
-        else: 
-            prefix = "Generate a long article based on title. Title: "
-            prompt = prefix+data["title"]+" Generation: "
-        
-        label = prompt+data["text"]
-        labels.append(label)
-    
-    calc_stats(logger, generations, labels)
 
     logger.info("calculating mauve scores...")
     mauve_score = mauve.compute(predictions=generations, references=labels)
     logger.info(f"mauve score: {mauve_score.mauve}")
     return mauve_score    
 
+def eval_bleu(logger,generations, labels):
+    """
+    Return bleu score of generations and references.
+    """
+    bleu = load("bleu")
+
+    logger.info("calculating mauve scores...")
+    bleu_scores = bleu.compute(predictions=generations, references=labels)
+    logger.info("mauve score: "+str(bleu_scores["bleu"]))
+    return bleu_scores
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--model",type=str, default="gpt2-large")
     parser.add_argument("--res_dir",type=str, default="baseline1/cc_news-output/")
-    parser.add_argument("--task",type=str, default="mauve")
+    parser.add_argument("--bleu",default=False, action="store_true")
+    parser.add_argument("--mauve",default=False,action="store_true")
     args = parser.parse_args()
-    
-    path = args.model + "-top-p"
-
-    path = args.res_dir+path
-
 
     handlers = [logging.StreamHandler()]
     logging.basicConfig(format='%(asctime)s - %(levelname)s - %(name)s - %(message)s',
@@ -96,10 +104,19 @@ if __name__ == '__main__':
                         level=logging.INFO,
                         handlers=handlers)
     logger = logging.getLogger(__name__)
+    
+    path = args.model + "-top-p"
+    path = args.res_dir+path
 
-    if args.task == "mauve":
-        res = eval_mauve(logger, data_path=path)
+    generations, labels = load_data_pair(logger,data_path=path)
+    calc_stats(logger, generations, labels)
 
+    if args.bleu:
+        res = eval_bleu(logger, generations, labels)
+
+    if args.mauve:
+        res = eval_mauve(logger, generations, labels)
+    
     
 
     
