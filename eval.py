59179a6ce3c99c94b335acb5c2c105e1d7c24545
Jiatong Yu <jiatongy@node211.ionic.cs.princeton.edu>
2022-12-16 19:24:41 -0500

    .

--- eval.py
+++ eval.py
@@ -10,7 +10,7 @@ from simCSE.simcse.tool import SimCSE
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
-def load_data_pair(logger, data_path):
+def load_data_pair(logger, data_path, gen_only = True ):
     """
     Load generation results, convert reference to prompt format. 
         data_path should contain res.npy and ref.npy files.
@@ -19,24 +19,34 @@ def load_data_pair(logger, data_path):
     """
     logger.info(f"loading data from {data_path}")
     assert os.path.exists(data_path+"-res.npy")
-    generations = np.load(data_path+"-res.npy")
-    references = np.load(data_path+"-ref.npy",allow_pickle=True)
+    raw_generations = np.load(data_path+"-res.npy")
+    raw_references = np.load(data_path+"-ref.npy",allow_pickle=True)
 
-    assert len(generations) == len(references)
+    assert len(raw_generations) == len(raw_references)
     
     labels=[]
-    for data in references:
-        if len(data["summary"]) > 1:
-            summary_chunck = data["summary"].split(sep="\n")[0].split(sep=". ")[0]+". "
-            prefix = "Generate long article based on title and summary. Title: "
-            prompt = prefix+data["title"]+" Summary: "+ summary_chunck+"Generation: "
+    generations = []
+    if gen_only:
+        for gen in raw_generations:
+            generations.append(gen.split(sep="Generation:")[1])
         
-        else: 
-            prefix = "Generate a long article based on title. Title: "
-            prompt = prefix+data["title"]+" Generation: "
-        
-        label = prompt+data["text"]
-        labels.append(label)
+        for lab in raw_references:
+            labels.append(lab["text"])
+
+    else: 
+        for data in raw_references:
+            if len(data["summary"]) > 1:
+                summary_chunck = data["summary"].split(sep="\n")[0].split(sep=". ")[0]+". "
+                prefix = "Generate long article based on title and summary. Title: "
+                prompt = prefix+data["title"]+" Summary: "+ summary_chunck+"Generation: "
+            
+            else: 
+                prefix = "Generate a long article based on title. Title: "
+                prompt = prefix+data["title"]+" Generation: "
+            
+            label = prompt+data["text"]
+            labels.append(label)
+        generations = raw_generations 
     
     assert(len(generations) == len(labels))
     return generations, labels
@@ -70,6 +80,9 @@ def calc_stats(logger, generations, labels):
     return 
     
 def eval_tfidf(logger, generations, labels):
+    """
+    Return tf-idf score.
+    """
     tfidf_vectorizer = TfidfVectorizer(use_idf=True)
     vectorizer = tfidf_vectorizer.fit(labels)
     logger.info("calculating tf-idf score...")
@@ -99,21 +112,24 @@ def eval_bleu(logger,generations, labels):
     Return bleu score of generations and references.
     """
     bleu = load("bleu")
-    scores = []
+    # scores = []
 
     logger.info("calculating bleu scores...")
-    for gen,ref in tqdm(zip(generations, labels)):
-        scores.append(bleu.compute(predictions=gen, references=ref))
-    
-    bleu_scores = np.average(scores)
-    logger.info("bleu score: "+str(bleu_scores))
-    return bleu_scores
+    try:
+        score = bleu.compute(predictions=generations, references=labels)
+    except ZeroDivisionError:
+        score = 0
+    #     scores.append(score)
+    # bleu_scores = np.average(scores)
+    logger.info("bleu score: "+str(score))
+    return score 
 
 def eval_simcse(logger, generations, labels):
     simcse = SimCSE("princeton-nlp/sup-simcse-bert-base-uncased")
     scores = []
+    logger.info("calculating simCSE scores...")
     for gen,ref in tqdm(zip(generations, labels)):
-        scores.append(simcse.similarity(gen,ref))
+        scores.append(simcse.similarity([gen],[ref]))
     
     sim_score = np.average(scores)
     return sim_score
@@ -126,6 +142,7 @@ if __name__ == '__main__':
     parser.add_argument("--bleu",default=False, action="store_true")
     parser.add_argument("--mauve",default=False,action="store_true")
     parser.add_argument("--simcse", default=False, action="store_true")
+    parser.add_argument("--tfidf",default=False,action="store_true")
 
     parser.add_argument("--log_folder", type=str, default="eval_log/")
     args = parser.parse_args()
@@ -137,13 +154,24 @@ if __name__ == '__main__':
                         handlers=handlers)
     logger = logging.getLogger(__name__)
     
-    path = args.model + "-top-p"
-    path = args.res_dir+path
+    # path = args.model + "-top-p"
+    # path = args.res_dir+path
+    MODEL_NAME = args.model
+    if MODEL_NAME in ["galactica-1.3b","galactica-6.7b"]:
+        NORMAL_PATH = f"baseline1/cc_news-output/{MODEL_NAME}-top-p"
+    else: 
+        NORMAL_PATH = f"baseline1/cc_news-output/var_len/{MODEL_NAME}-300-FINAL"
+        ADV_PATH = f"baseline1/cc_news-output/var_len/{MODEL_NAME}-300-adv"
+        IN_CONTEXT_PATH = f"baseline1/cc_news-output/var_len/{MODEL_NAME}-300-INCONTEXT"
 
-    generations, labels = load_data_pair(logger,data_path=path)
+    generations, labels = load_data_pair(logger,data_path=NORMAL_PATH)
     calc_stats(logger, generations, labels)
 
     res_dict = {}
+
+    if not isinstance(generations, list):
+        generations = [generations]
+        labels = [labels]
 
     if args.bleu:
         res = eval_bleu(logger, generations, labels)
@@ -154,8 +182,13 @@ if __name__ == '__main__':
         res_dict["mauve"] = res 
     
     if args.simcse: 
-        res = eval_bleu(logger, generations, labels)
+        res = eval_simcse(logger, generations, labels)
         res_dict["simcse"] = res
     
+    if args.tfidf: 
+        res = eval_tfidf(logger, generations, labels)
+        res_dict["tfidf"] = res
+    
+    print(res_dict)
     
 
